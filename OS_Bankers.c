7ccfc76c3c03aa798d6210bcad8e154e70f1c3db
Parshva Choradia <112927478+Parsh092@users.noreply.github.com>
2023-01-06 00:57:26 +0530

    Output added

--- OS_Bankers.c
+++ OS_Bankers.c
@@ -100,3 +100,89 @@ else
 	printf("\nThe system is in an unsafe state!!");
 getch();
 }
+
+
+
+
+
+
+
+
+
+
+
+output: - 
+Enter the no of processes : 5
+
+
+Enter the no of resources : 3
+
+
+Enter the Max Matrix for each process : 
+For process 1 : 7 5 3
+
+For process 2 : 3 2 2
+
+For process 3 : 9 0 2
+
+For process 4 : 2 2 2
+
+For process 5 : 4 3 3
+
+
+Enter the allocation for each process :
+For process 1 : 0 1 0
+
+For process 2 : 2 0 0
+
+For process 3 : 3 0 2
+
+For process 4 : 2 1 1
+
+For process 5 : 0 0 2
+
+
+Enter the Available Resources : 3 3 2
+
+ Max matrix:    Allocation matrix:
+7  5  3                 0  1  0
+3  2  2                 2  0  0
+9  0  2                 3  0  2
+2  2  2                 2  1  1
+4  3  3                 0  0  2
+
+Process 2 runs to completion!
+ Max matrix:    Allocation matrix:
+7  5  3                 0  1  0
+0  0  0                 0  0  0
+9  0  2                 3  0  2
+2  2  2                 2  1  1  
+4  3  3                 0  0  2
+
+Process 4 runs to completion!
+ Max matrix:    Allocation matrix:
+7  5  3                 0  1  0
+0  0  0                 0  0  0
+9  0  2                 3  0  2
+0  0  0                 0  0  0
+4  3  3                 0  0  2
+
+Process 1 runs to completion!
+ Max matrix:    Allocation matrix:
+0  0  0                 0  0  0
+0  0  0                 0  0  0
+9  0  2                 3  0  2  
+0  0  0                 0  0  0
+4  3  3                 0  0  2
+
+Process 3 runs to completion!
+ Max matrix:    Allocation matrix:
+0  0  0                 0  0  0
+0  0  0                 0  0  0
+0  0  0                 0  0  0
+0  0  0                 0  0  0
+4  3  3                 0  0  2
+
+Process 5 runs to completion!
+The system is in a safe state!!
+Safe Sequence : < 2  4  1  3  5  >
