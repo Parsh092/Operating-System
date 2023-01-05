bc3f03f5f398d508bb9da3d87221f3272412f38e
Parshva Choradia <112927478+Parsh092@users.noreply.github.com>
2023-01-06 01:12:11 +0530

    Update process_management.c

--- process_management.c
+++ process_management.c
@@ -16,3 +16,32 @@ int main (){
   printf("The value of n is %d\n",n); 
     return 0;
 }
+
+
+
+// New Code
+
+#include<stdio.h>
+#include<sys/wait.h>
+#include<unistd.h>
+int main()
+{
+int a;
+printf("Hello\n");
+printf("I am EXEC.c called by execvp()");
+printf("In fork command");
+a=fork();
+if (a>0)
+{
+printf("Parent pid is %d\n", getpid());
+printf("{arent ppid is %d\n", getppid());
+}
+if (a==0)
+{
+printf("Child pid is %d\n", getpid());
+printf("Child ppid is %d\n", getppid());
+}
+printf("Work Done\n");
+sleep(4);
+printf("Work Accomplished");
+return 0;
